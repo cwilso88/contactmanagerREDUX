6088c076c052de8692c640ba604c0df114b5f066
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-07-24 20:10:19 -0400

    changed the logo

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import logo from "./logo.png";
+import logo from "./logored.png";
 
 const Header = props => {
   const { branding } = props;
