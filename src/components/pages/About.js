2723ac5a5ee55409a6e613b2e389854466c90461
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-07-25 00:27:14 -0400

    about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default () => {
   return (
-    <div>
-      <h1 className="display-4">About Contact Manager</h1>
-      <p className="lead">Simple app to manage contacts</p>
+    <div style={{ height: "90vh"}}>
+      <h1 className="display-4">About Lyon</h1>
+      <p className="lead">Simple app to manage the team members of each department of the tech consulting company.</p>
       <p className="text-secondary">Version 1.0.0</p>
     </div>
   );
