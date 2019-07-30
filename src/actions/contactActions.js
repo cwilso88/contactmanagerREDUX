90c5bf38cc6399630e7ee5a4b64e9899ddcc507d
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-07-30 06:12:55 -0400

    Added middleware cors-anywhere.heroku.app.com before each api endpoint

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -9,43 +9,44 @@ import {
 import axios from 'axios';
 
 
+
 export const getContacts = () => async dispatch => {
-    const res = await axios.get('https://jsonserverapi.firebaseapp.com/api/users');
+    const res = await axios.get('https://cors-anywhere.herokuapp.com/https://jsonserverapi.firebaseapp.com/api/users');
     dispatch({
         type: GET_CONTACTS,
         payload: res.data
     });
 };
 export const getGoals = () => async dispatch => {
-    const res = await axios.get('https://jsonserverapi.firebaseapp.com/api/departments');
+    const res = await axios.get('https://cors-anywhere.herokuapp.com/https://jsonserverapi.firebaseapp.com/api/departments');
     dispatch({
         type: GET_GOALS,
         payload: res.data
     });
 };
 export const getContact = (id) => async dispatch => {
-    const res = await axios.get(`https://jsonserverapi.firebaseapp.com/api/users/${id}`);
+    const res = await axios.get(`https://cors-anywhere.herokuapp.com/https://jsonserverapi.firebaseapp.com/api/users/${id}`);
     dispatch({
         type: GET_CONTACT,
         payload: res.data
     });
 };
 export const deleteContact = (id) => async dispatch => {
-    await axios.delete(`https://jsonserverapi.firebaseapp.com/api/users/${id}`);
+    await axios.delete(`https://cors-anywhere.herokuapp.com/https://jsonserverapi.firebaseapp.com/api/users/${id}`);
     dispatch({
         type: DELETE_CONTACT,
         payload: id
     });
 };
 export const addContact = (contact) => async dispatch => {
-    const res = await axios.post('https://jsonserverapi.firebaseapp.com/api/users', contact);
+    const res = await axios.post('https://cors-anywhere.herokuapp.com/https://jsonserverapi.firebaseapp.com/api/users', contact);
     dispatch({
         type: ADD_CONTACT,
         payload: res.data
     });
 };
 export const updateContact = (contact) => async dispatch => {
-    const res = await axios.patch(`https://jsonserverapi.firebaseapp.com/api/users/${contact.id}`, contact);
+    const res = await axios.patch(`https://cors-anywhere.herokuapp.com/https://jsonserverapi.firebaseapp.com/api/users/${contact.id}`, contact);
     dispatch({
         type: UPDATE_CONTACT,
         payload: res.data
